d96219a4f5b8d1ede01ca7cae8d94776c9ee6e0a
Jayanth-M0625 <147526637+Jayanth-M0625@users.noreply.github.com>
2024-05-16 23:16:22 +0530

    Solutions to Rustlings exercises till Hashmaps

---
+++ rustlings/exercises/variables/variables6.rs
@@ -0,0 +1,12 @@
+// variables6.rs
+//
+// Execute `rustlings hint variables6` or use the `hint` watch subcommand for a
+// hint.
+
+
+
+const NUMBER: u32 = 3;
+fn main() {
+    const NUMBER: u32 = 3;
+    println!("Number {}", NUMBER);
+}
