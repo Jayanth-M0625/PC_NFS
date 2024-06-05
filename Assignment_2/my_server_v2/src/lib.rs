a087a699545e1620a6b3c002b868d175c4abf919
Jayanth-M0625 <147526637+Jayanth-M0625@users.noreply.github.com>
2024-06-05 17:10:37 +0530

    Alittle cleaner

--- Assignment_2/my_server_v2/src/lib.rs
+++ Assignment_2/my_server_v2/src/lib.rs
@@ -1,34 +1,40 @@
-// use std::net::*;
-// use std::io::{BufRead, Read, Write};
-// use std::fs;
-// fn handle_connection(mut stream: TcpStream) {
-//     println!("Connection established!");
-//     let mut rdr = std::io::BufReader::new(&mut stream);
-//     let mut request = String::new(); 
-//     loop{
-//         let mut lines = String::new();
-//         rdr.read_line(&mut lines).unwrap();
-//         if lines.trim().is_empty() {break;}
-//         request.push_str(&lines);
-//     }
-//     let response = response(&request); 
-//     stream.write_all(response.as_bytes()).unwrap();
-// }
-// fn response(request: &String) -> String{
-//     println!("Request from client:\n{}", request);
-//     let get = "GET / HTTP/1.1\r\n";
-//     let (status, file_path) = if request.starts_with(get) {
-//         ("HTTP/1.1 200 OK", "index.html")
-//     }
-//     else {
-//         ("HTTP/1.1 404 NOT FOUND", "404.html")
-//     }
-//     let content = fs::read_to_string(file_path).unwrap();
-//     let response = format!(
-//         "{}\r\nContent-Length: {}\r\n\r\n{}",
-//         status,
-//         content.len(),
-//         content
-//         );
-//     response
-// }
+
+
+pub mod server_deps{
+    use std::net::*;
+    use std::io::{BufRead, Read, Write};
+    use std::fs;
+
+    pub fn handle_connection(mut stream: TcpStream) {
+        println!("Connection established!");
+        let mut rdr = std::io::BufReader::new(&mut stream);
+        let mut request = String::new(); 
+        loop{
+            let mut lines = String::new();
+            rdr.read_line(&mut lines).unwrap();
+            if lines.trim().is_empty() {break;}
+            request.push_str(&lines);
+        }
+        let response = response(&request); 
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+    
+    pub fn response(request: &String) -> String{
+        println!("Request from client:\n{}", request);
+        let get = "GET / HTTP/1.1\r\n";
+        let (status, file_path) = if request.starts_with(get) {
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
+        let content = fs::read_to_string(file_path).unwrap();
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status,
+            content.len(),
+            content
+            );
+        response
+    }
+}
--- Assignment_2/my_server_v2/src/main.rs
+++ Assignment_2/my_server_v2/src/main.rs
@@ -1,42 +1,9 @@
 #[allow(unused_variables)]
+
+use my_Server_v2::server_deps::*;
 use std::net::*;
 use std::io::{BufRead, Read, Write};
 use std::fs;
-
-// use std::fmt::Display;
-
-fn handle_connection(mut stream: TcpStream) {
-    println!("Connection established!");
-    let mut rdr = std::io::BufReader::new(&mut stream);
-    let mut request = String::new(); 
-    loop{
-        let mut lines = String::new();
-        rdr.read_line(&mut lines).unwrap();
-        if lines.trim().is_empty() {break;}
-        request.push_str(&lines);
-    }
-    let response = response(&request); 
-    stream.write_all(response.as_bytes()).unwrap();
-}
-fn response(request: &String) -> String{
-    println!("Request from client:\n{}", request);
-    let get = "GET / HTTP/1.1\r\n";
-    let (status, file_path) = if request.starts_with(get) {
-        ("HTTP/1.1 200 OK", "index.html")
-    }
-    else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
-    };
-let content = fs::read_to_string(file_path).unwrap();
-        let response = format!(
-            "{}\r\nContent-Length: {}\r\n\r\n{}",
-            status,
-            content.len(),
-            content
-        );
-        response
-}
-    
 
 fn main() {
     
